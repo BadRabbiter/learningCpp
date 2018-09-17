f666fb51bbed550846447d14e03d694227f4cbe1
BadRabbit <37303767+BadRabbiter@users.noreply.github.com>
2018-09-17 16:56:59 +0800

    Add files via upload
    char数组不能通过将指针赋值给数组名修改；

---
+++ person.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+class Person {
+private:
+	static const int LIMIT = 25;
+	std::string lname;
+	char fname[LIMIT];
+public:
+	Person() { lname = ""; fname[0] = '\0'; }
+	Person(const std::string &ln, const char *fn = "Heyyou");
+	void show() const;
+	void formalShow() const;
+};
